4d430d8bdf0f237ed1d63f96f6524d604bdb68ab
Grancalavera <leoncoto@gmail.com>
2016-10-15 20:41:00 +0000

    remove superfluous function and comments around seq

--- Main.hs
+++ Main.hs
@@ -10,9 +10,7 @@ main = do
 
 writeWith f inpf outf = do
   input <- readFile inpf
-  -- because of lazyness, thus forcing strictness
-  -- http://stackoverflow.com/a/2530948/824779
-  id input `seq` (writeFile outf $ f input)
+  input `seq` (writeFile outf $ f input)
 
 fixLines cs = unlines (map trimTrailWtSpc $ lines cs)
 
