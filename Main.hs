fbf9e00da3a4a7147f6d882a4c1f3060bf601c8a
Grancalavera <leoncoto@gmail.com>
2016-10-15 11:30:31 +0000

    add scaffolding for file overwrite

--- Main.hs
+++ Main.hs
@@ -1,15 +1,22 @@
 module Main where
 import System.Environment (getArgs)
+import System.IO (openFile, IOMode)
 
 main = do
   args <- getArgs
   case args of
-    [inp,out] -> interactWith fixLines inp out
-    _         -> putStrLn "error: exactly 2 args required"
+    [inp,out] -> writeWith fixLines inp out
+    [inp]     -> overwriteWith fixLines inp
+    _         -> errorMsg "not enough arguments"
+
+writeWith f inpf outf
+  | inpf == outf = overwriteWith f inpf
+  | otherwise    = do 
+                     input <- readFile inpf
+                     writeFile outf (f input)
     
-interactWith f inpf outf = do
-  input <- readFile inpf
-  writeFile outf (f input)
+overwriteWith f file = do
+  putStrLn $ "file \"" ++ file ++ "\" overwritten" 
 
 fixLines cs = unlines (map trimTrailWtSpc $ lines cs)
 
@@ -21,3 +28,15 @@ trimLeadWtSpc (' ':cs)  = trimLeadWtSpc cs
 trimLeadWtSpc ('\t':cs) = trimLeadWtSpc cs
 trimLeadWtSpc cs        = cs
 
+errorMsg msg = do
+  putStrLn $ msg
+    ++  "\nusage:"
+    ++  "\ntrimspaces <input> <output>  trim trailing"
+    ++  "\n                             whitespace from"
+    ++  "\n                             <input> and write"
+    ++  "\n                             result to <output>"
+    ++  "\ntrimspaces <input>           trim trailing"
+    ++  "\n                             whitespace from"
+    ++  "\n                             <input> and" 
+    ++  "\n                             overwrite result"
+    ++  "\n                             back to <input>"
