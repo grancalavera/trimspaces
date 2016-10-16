5041a173002c29cfe8cf8e5ac47fea1f8b42a3d3
Grancalavera <leoncoto@gmail.com>
2016-10-16 12:29:29 +0000

    restore warning after file  overwrite

--- Main.hs
+++ Main.hs
@@ -4,13 +4,17 @@ import System.Environment (getArgs)
 main = do
   args <- getArgs
   case args of
+    [inp]     -> overwriteWith fixLines inp
     [inp,out] -> writeWith fixLines inp out
-    [inp]     -> writeWith fixLines inp inp
     _         -> usage "not enough arguments"
 
 writeWith f inpf outf = do
   input <- readFile inpf
   input `seq` (writeFile outf $ f input)
+
+overwriteWith f inpf = do
+  writeWith f inpf inpf
+  putStrLn $ "file \"" ++ inpf ++ "\" ovewritten"
 
 fixLines cs = unlines (map trimTrailWtSpc $ lines cs)
 
